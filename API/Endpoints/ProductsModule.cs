9d7c7ff26cdbefb3840bd037dc2dbbb74d85f062
John Cassidy <jpcassidy@outlook.com>
2024-01-18 13:55:53 -0500

    Add Carter package and configure Carter endpoints

---
+++ API/Endpoints/ProductsModule.cs
@@ -0,0 +1,29 @@
+﻿using Carter;
+
+namespace API.Endpoints;
+
+public class ProductsModule : CarterModule
+{
+    public ProductsModule() : base("/api/products")
+    {
+    }
+    public override void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/", GetProducts).WithName(nameof(GetProducts));
+        app.MapGet("/{id}", (int id) => $"Product {id}").WithName("GetProductById");
+    }
+
+    private static async Task<IResult> GetProducts()
+    {
+        try
+        {
+            var products = await Task.FromResult(new[] { "product 1", "product 2" });
+            return Results.Ok(products);
+        }
+        catch (Exception ex)
+        {
+            // Handle the exception here
+            return Results.BadRequest(ex.Message);
+        }
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using API.Endpoints;
+using Carter;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,6 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCarter();
 
 var app = builder.Build();
 
@@ -19,10 +22,6 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.AddWeatherForecastEndpoints();
+app.MapCarter();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
