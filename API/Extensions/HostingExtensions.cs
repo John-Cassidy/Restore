d8155a3a257e4a43303f0b8193eda2bc8fb3b449
John Cassidy <jpcassidy@outlook.com>
2024-01-18 15:00:18 -0500

    Update API configuration and logging settings

---
+++ API/Extensions/HostingExtensions.cs
@@ -0,0 +1,36 @@
+﻿using API.Endpoints;
+using Carter;
+
+namespace API.Extensions;
+
+public static class HostingExtensions
+{
+    public static void ConfigureServices(this WebApplicationBuilder builder)
+    {
+        // Add services to the container.
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        builder.Services.AddCarter();
+    }
+
+    public static WebApplication ConfigurePipeline(this WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        if (app.Environment.IsProduction())
+        {
+            app.UseHttpsRedirection();
+        }
+
+        app.AddWeatherForecastEndpoints();
+        app.MapCarter(); // app.MapFallbackToController("Index", "Fallback");
+
+        return app;
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,27 +1,13 @@
 using API.Endpoints;
+using API.Extensions;
 using Carter;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddCarter();
+builder.ConfigureServices();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.AddWeatherForecastEndpoints();
-app.MapCarter();
+app.ConfigurePipeline();
 
 app.Run();
