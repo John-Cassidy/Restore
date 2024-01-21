7cc746bf8d8457b40feca11de48d8a0333e5f21d
John Cassidy <jpcassidy@outlook.com>
2024-01-21 16:43:50 -0500

    add CORS to API to allow client app access and fetch products from server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,36 +1,38 @@
-import { useState } from 'react';
-
-function generateProducts() {
-  const products = [];
-  for (let i = 1; i <= 3; i++) {
-    const product = {
-      name: `product${i}`,
-      price: i * 100.0,
-    };
-    products.push(product);
-  }
-  return products;
-}
+import { useEffect, useState } from 'react';
 
 export const App = () => {
-  const [products, setProducts] = useState(generateProducts());
+  const [products, setProducts] = useState({
+    metaData: {
+      pageNumber: 1,
+      pageSize: 10,
+      totalPages: 1,
+      totalCount: 0,
+    },
+    data: [] as any[],
+  });
 
-  const addProducts = () => {
-    setProducts((prevState) => [
-      ...prevState,
-      {
-        name: `product${prevState.length + 1}`,
-        price: prevState.length * 100 + 100.0,
-      },
-    ]);
-  };
+  useEffect(() => {
+    fetch('http://localhost:5000/products?PageNumber=1&PageSize=10')
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+
+  // const addProducts = () => {
+  //   setProducts((prevState) => [
+  //     ...prevState,
+  //     {
+  //       name: `product${prevState.length + 1}`,
+  //       price: prevState.length * 100 + 100.0,
+  //     },
+  //   ]);
+  // };
 
   return (
     <div>
       <h1>Re-Store</h1>
-      <button onClick={addProducts}>Add products</button>
+      {/* <button onClick={addProducts}>Add products</button> */}
       <ul>
-        {products.map((product) => (
+        {products.data.map((product) => (
           <li key={product.name}>
             {product.name} - {product.price}
           </li>
