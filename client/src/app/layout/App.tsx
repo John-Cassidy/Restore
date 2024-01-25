3f280e8041ae2aa075ec0d9fe4b29bb7ccffe2b9
John Cassidy <jpcassidy@outlook.com>
2024-01-25 14:42:04 -0500

    Add loading component and fetch basket on app load

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -6,13 +6,30 @@ import {
   ThemeProvider,
   createTheme,
 } from '@mui/material';
+import { useEffect, useState } from 'react';
 
 import { Header } from './Header';
+import { LoadingComponent } from './LoadingComponent';
 import { Outlet } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
-import { useState } from 'react';
+import { agent } from '../api/agent';
+import { getCookie } from '../util/util';
+import { useStoreContext } from '../context/StoreContext';
 
 export const App = () => {
+  const { setBasket } = useStoreContext();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const buyerId = getCookie('buyerId');
+    if (buyerId) {
+      agent.Basket.get()
+        .then((basket) => setBasket(basket))
+        .catch((error) => console.log(error))
+        .finally(() => setLoading(false));
+    }
+  }, [setBasket]);
+
   const [darkMode, setDarkMode] = useState(true);
   const palleteType = darkMode ? 'dark' : 'light';
   const theme = createTheme({
@@ -27,6 +44,9 @@ export const App = () => {
   const handleThemeChange = () => {
     setDarkMode(!darkMode);
   };
+
+  if (loading)
+    return <LoadingComponent message='Loading basket...'></LoadingComponent>;
 
   return (
     <div>
